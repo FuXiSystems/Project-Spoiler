5d2ef65fc81767df23c692c5d9bf4a0a83b3a5ab
Mohamed <mohamed20884@yahoo.com>
2015-02-26 14:19:51 +0000

    added database dump

--- api.py
+++ api.py
@@ -32,8 +32,8 @@ from sqlalchemy.orm import backref, relationship
 # Create the Flask application.
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://fuxiadmin:admin123@fuxi.cjbrs9qrutbu.us-west-2.rds.amazonaws.com:5432/wms'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:casper@localhost/wms'
+#app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://fuxiadmin:admin123@fuxi.cjbrs9qrutbu.us-west-2.rds.amazonaws.com:5432/wms'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5400/wms'
 
 # Create the db Connection
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
